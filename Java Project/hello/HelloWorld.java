076c6ecb67933292e8cbadc23ec15f58af5817d0
wangjona <wangjona@grinnell.edu>
2023-09-04 11:14:16 -0500

    first commit

---
+++ Java Project/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.io.PrintWriter;
+
+public class HelloWorld{
+    public static void main (String[] args) {
+        PrintWriter pen = new PrintWriter(System.out, true);
+        pen.println ("Hello, GitHub!");
+        pen.flush();
+      } // main(String[])
+}
+
